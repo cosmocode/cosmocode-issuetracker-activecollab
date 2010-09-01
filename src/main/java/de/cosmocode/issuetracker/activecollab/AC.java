25cc6a723b16f64201688aead4ae009fe70d0f2c
sarnowski <sarnowski@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-09-01 11:11:13 +0000

    initial httpclient and jackson integration

--- src/main/java/de/cosmocode/issuetracker/activecollab/AC.java
+++ src/main/java/de/cosmocode/issuetracker/activecollab/AC.java
@@ -16,14 +16,33 @@
 
 package de.cosmocode.issuetracker.activecollab;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import de.cosmocode.issuetracker.AbstractIssueTracker;
 import de.cosmocode.issuetracker.Issue;
-import de.cosmocode.issuetracker.IssueTracker;
+import de.cosmocode.issuetracker.IssueTrackerException;
+import de.cosmocode.issuetracker.StoringIssueFailed;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Tobias Sarnowski
@@ -32,17 +51,36 @@ final class AC extends AbstractIssueTracker implements ActiveCollab {
     private static final Logger LOG = LoggerFactory.getLogger(AC.class);
 
     private final URI uri;
+    private final URI apiUri;
     private final String user;
     private final String apiKey;
     private final int projectId;
     private int milestoneId = ActiveCollab.NO_ID;
     private int parentId = ActiveCollab.NO_ID;
 
+    private final HttpClient httpclient;
+    private final ObjectMapper mapper = new ObjectMapper();
+
     AC(URI uri, String user, String apiKey, int projectId) {
         this.uri = uri;
         this.user = user;
         this.apiKey = apiKey;
         this.projectId = projectId;
+
+        // add path to api.php to the given URI
+        String url = uri.toString();
+        if (!url.endsWith("/")) {
+            url = url + "/";
+        }
+        url = url + "public/api.php";
+        try {
+            this.apiUri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        // initialize the http clients
+        httpclient = new DefaultHttpClient();
     }
 
     @Override
@@ -81,18 +119,39 @@ final class AC extends AbstractIssueTracker implements ActiveCollab {
     }
 
     @Override
-    public Issue createIssue(String title, String description) {
-        return null;
+    public Issue createIssue(String title, String description) throws IssueTrackerException {
+        final String pathInfo = "";
+        final Map<String,String> parameters = Maps.newHashMap();
+
+        try {
+            return parseTicket(requestPost(pathInfo, parameters));
+        } catch (IOException e) {
+            throw new StoringIssueFailed(e);
+        }
     }
 
     @Override
-    public List<Issue> listIssues() {
-        throw new UnsupportedOperationException();
+    public List<? extends Issue> listIssues() throws IssueTrackerException {
+        final String pathInfo = "";
+        final Map<String,String> parameters = Maps.newHashMap();
+
+        try {
+            return parseTickets(requestPost(pathInfo, parameters));
+        } catch (IOException e) {
+            throw new StoringIssueFailed(e);
+        }
     }
 
     @Override
-    public void updateIssue(Issue issue) {
-        throw new UnsupportedOperationException();
+    public Issue updateIssue(Issue issue) throws IssueTrackerException {
+        final String pathInfo = "";
+        final Map<String,String> parameters = Maps.newHashMap();
+
+        try {
+            return parseTicket(requestPost(pathInfo, parameters));
+        } catch (IOException e) {
+            throw new StoringIssueFailed(e);
+        }
     }
 
     @Override
@@ -102,6 +161,51 @@ final class AC extends AbstractIssueTracker implements ActiveCollab {
                 ", user='" + user + '\'' +
                 ", projectId=" + projectId +
                 '}';
+    }
+
+    private JsonNode requestGet(String pathInfo) throws IOException {
+        HttpGet get = new HttpGet();
+        return request(get, pathInfo);
+    }
+
+    private JsonNode requestPost(String pathInfo, Map<String,String> parameters) throws IOException {
+        HttpPost post = new HttpPost();
+
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(parameters.size() + 1);
+        nameValuePairs.add(new BasicNameValuePair("submitted", "submitted"));
+        for (Map.Entry<String,String> entry: parameters.entrySet()) {
+            nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+        }
+        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+        return request(post, pathInfo);
+    }
+
+    private JsonNode request(HttpRequestBase request, String pathInfo) throws IOException {
+        final URI uri;
+        try {
+            uri = new URI(apiUri.toString() + "?path_info=" + pathInfo);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
+        request.setURI(uri);
+
+        request.setHeader("Accept", "application/json");
+
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        String response = httpclient.execute(request, responseHandler);
+
+        return mapper.readValue(response, JsonNode.class);
+    }
+
+    private List<ActiveCollabIssue> parseTickets(JsonNode json) {
+        List<ActiveCollabIssue> issues = Lists.newArrayList();
+        json.toString();
+        return issues;
+    }
+
+    private ActiveCollabIssue parseTicket(JsonNode json) {
+        return null;
     }
 
 }
